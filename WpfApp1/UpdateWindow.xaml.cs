6b9dbb16c32e3d5f7ea6ce59b07905a89b69ec2c
Christopher <christopherkarlsson95@gmail.com>
2021-07-19 04:01:27 +0200

    Update-Commit

--- WpfApp1/UpdateWindow.xaml.cs
+++ WpfApp1/UpdateWindow.xaml.cs
@@ -4,13 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Squirrel;
 
 namespace TwitchTrack
 {
@@ -19,11 +13,25 @@ namespace TwitchTrack
     /// </summary>
     public partial class UpdateWindow : Window
     {
-        public UpdateWindow(NuGet.SemanticVersion currentVersion, NuGet.SemanticVersion availableVersion)
+        UpdateManager manager;
+        public UpdateWindow(NuGet.SemanticVersion currentVersion, NuGet.SemanticVersion availableVersion, UpdateManager manager)
         {
             InitializeComponent();
+            this.manager = manager;
             CurrentVersionLabel.Content = "Version: " + currentVersion;
             AvailableVersionLabel.Content = "Available: " + availableVersion;
+        }
+
+        private async void Update(object sender, RoutedEventArgs e)
+        {
+            await manager.UpdateApp();
+            CurrentVersionLabel.Content = manager.CurrentlyInstalledVersion();
+            AvailableVersionLabel.Content = "Successfully updated! Turn off the application and start it again for the update to take effect.";
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
