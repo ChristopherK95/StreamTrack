843a61da4a8a0760875a10dec9ebe70845242e7b
Christopher <christopherkarlsson95@gmail.com>
2021-07-20 11:54:25 +0200

    UpdateWindow readjustments
    -Update window now looks a bit better and text should no longer get clipped.

--- WpfApp1/UpdateWindow.xaml.cs
+++ WpfApp1/UpdateWindow.xaml.cs
@@ -19,14 +19,21 @@ namespace TwitchTrack
             InitializeComponent();
             this.manager = manager;
             CurrentVersionLabel.Content = "Version: " + currentVersion;
-            AvailableVersionLabel.Content = "Available: " + availableVersion;
+            AvailableVersionLabel.Text = "Available: " + availableVersion;
         }
 
         private async void Update(object sender, RoutedEventArgs e)
         {
+            AvailableVersionLabel.Visibility = Visibility.Hidden;
+            LoadingCog.Visibility = Visibility.Visible;
             await manager.UpdateApp();
+            LoadingCog.Visibility = Visibility.Hidden;
+            AvailableVersionLabel.Visibility = Visibility.Visible;
+
             CurrentVersionLabel.Content = manager.CurrentlyInstalledVersion();
-            AvailableVersionLabel.Content = "Successfully updated! Turn off the application and start it again for the update to take effect.";
+            AvailableVersionLabel.Text = "Successfully updated! Restart the application for the update to take effect.";
+            AvailableVersionLabel.FontSize = 18;
+            AvailableVersionLabel.FontWeight = FontWeights.DemiBold;
         }
 
         private void Cancel(object sender, RoutedEventArgs e)
